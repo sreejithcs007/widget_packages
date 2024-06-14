b1e6ae5f5e8c1418728903f5160f78a948b9015c
sreejithcs007 <sreejithcs365@gmail.com>
2024-06-14 13:09:19 +0530

    added dropdown,search field,textform,login field

---
+++ lib/src/sreejith/src/loginfield/custom_loginfield.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:nexteon_group_packages/src/sajin/core/const/my_text_style.dart';
+import 'package:nexteon_group_packages/src/sajin/text/custom_text.dart';
+import 'package:nexteon_group_packages/src/sreejith/src/textformfield/custom_textfield.dart';
+
+class CustomLoginTextField extends StatelessWidget {
+  const CustomLoginTextField(
+      {super.key,
+      required this.text,
+      this.controller,
+      this.hintText,
+      this.color,
+      this.fontSize,
+      this.fontWeight,
+      this.style,
+      this.hintcolor,
+      this.validator});
+
+  final String text;
+  final TextEditingController? controller;
+  final String? hintText;
+  final Color? color;
+  final double? fontSize;
+  final FontWeight? fontWeight;
+  final TextStyle? style;
+  final Color? hintcolor;
+  final String? Function(String?)? validator;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CustomText(
+          text: text,
+          style: TextStyle(
+                  color: color ?? Theme.of(context).colorScheme.tertiary,
+                  fontSize: fontSize ?? MyTextSTyles.greyButtonText.fontSize,
+                  fontWeight:
+                      fontWeight ?? MyTextSTyles.greyButtonText.fontWeight)
+              .merge(style),
+        ),
+
+        ///need to do gap
+        const SizedBox(
+          height: 10,
+        ),
+
+        CustomTextFormField(
+          validator: validator,
+          enabled: true,
+          controller: controller,
+          hintText: hintText,
+          hintStyle: TextStyle(
+              color: hintcolor ?? Theme.of(context).colorScheme.onSecondary),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/src/sreejith/src/textformfield/custom_textfield.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:nexteon_group_packages/src/sajin/core/const/my_text_style.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  const CustomTextFormField({
+    super.key,
+    this.controller,
+    this.autocorrect,
+    this.autofocus,
+    this.validator,
+    this.hintText,
+    this.maxLength,
+    this.border,
+    this.maxLine,
+    this.alignLabelWithHint,
+    this.constraints,
+    this.contentPadding,
+    this.counter,
+    this.counterStyle,
+    this.counterText,
+    this.disabledBorder,
+    this.enabled,
+    this.suffix,
+    this.suffixIcon,
+    this.prefix,
+    this.prefixIcon,
+    this.filled,
+    this.filledColor,
+    this.obsecureText,
+    this.obscuringCharacter,
+    this.minLines,
+    this.hintStyle,
+    this.color,
+    this.fontSize,
+    this.fontWeight,
+    this.borderColor,
+    this.keyboardType,
+    this.inputFormatters,
+    this.IsEditable = true,
+    this.hintcolor,
+    // this.style,
+  });
+
+  final bool? IsEditable;
+  final TextEditingController? controller;
+  final bool? autocorrect;
+  final bool? autofocus;
+  final String? Function(String?)? validator;
+  final String? hintText;
+  final int? maxLength;
+  final InputBorder? border;
+  final int? maxLine;
+  final bool? alignLabelWithHint;
+  final BoxConstraints? constraints;
+  final EdgeInsetsGeometry? contentPadding;
+  final Widget? counter;
+  final TextStyle? counterStyle;
+  final String? counterText;
+  final InputBorder? disabledBorder;
+  final bool? enabled;
+  final String? suffix;
+  final Widget? suffixIcon;
+  final String? prefix;
+  final Widget? prefixIcon;
+  final bool? filled;
+  final Color? filledColor;
+  final bool? obsecureText;
+  final String? obscuringCharacter;
+  final int? minLines;
+  final TextStyle? hintStyle;
+  final Color? color;
+  final double? fontSize;
+  final FontWeight? fontWeight;
+  final Color? borderColor;
+  final TextInputType? keyboardType;
+  final List<TextInputFormatter>? inputFormatters;
+  final Color? hintcolor;
+  // final TextStyle? style;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      style: TextStyle(
+        fontSize: fontSize ?? MyTextSTyles.blackButtonText.fontSize,
+        fontWeight: fontWeight ?? MyTextSTyles.blackButtonText.fontWeight,
+      ),
+      controller: controller,
+      keyboardType: keyboardType ?? TextInputType.emailAddress,
+      inputFormatters: inputFormatters,
+      validator: validator,
+      obscureText: obsecureText ?? false,
+      obscuringCharacter: obscuringCharacter ?? "*",
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: TextStyle(
+                color: hintcolor ?? Theme.of(context).colorScheme.onSecondary,
+                fontSize: fontSize ?? MyTextSTyles.whiteButtonText.fontSize,
+                fontWeight:
+                    fontWeight ?? MyTextSTyles.whiteButtonText.fontWeight)
+            .merge(hintStyle),
+        suffix: Text(suffix ?? ""),
+        suffixIcon: suffixIcon,
+        prefix: Text(prefix ?? ""),
+        prefixIcon: prefixIcon,
+        filled: filled ?? false,
+        fillColor: filledColor,
+        border: border ??
+            OutlineInputBorder(
+                borderSide: BorderSide(
+                    color: Theme.of(context).colorScheme.onSecondary),
+                borderRadius: BorderRadius.circular(5)),
+        alignLabelWithHint: alignLabelWithHint,
+        constraints: constraints,
+        contentPadding: contentPadding,
+        counter: counter,
+        counterStyle: counterStyle,
+        counterText: counterText,
+        disabledBorder: disabledBorder,
+        enabled: enabled ?? true,
+      ),
+      minLines: minLines,
+      maxLength: maxLength,
+      maxLines: maxLine,
+      autocorrect: autocorrect ?? true,
+      autofocus: autofocus ?? false,
+    );
+  }
+}
